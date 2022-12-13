7b2aecc0ad3f8c9c9fb6cd98d10df989cc0954c7
Anthony Martinez <aenriquemartinez@gmail.com>
2022-12-12 21:22:09 -0500

    compoletada integracion de las partes de la vista general

--- js/main.js
+++ js/main.js
@@ -3,28 +3,39 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobile = document.querySelector('.menu');
 const toggleMobile = document.querySelector('.mobile-menu');
 const shopingCar = document.querySelector('.navbar-shopping-cart');
-const shoppingDetails = document.querySelector('.product-detail');
+const shoppingDetails = document.querySelector('#shoppingCarContainer');
+const productDetailContainer = document.querySelector('#productDetails');
+const productDetailClose = document.querySelector('.product-detail-close');
 const cardsContainer = document.querySelector('.cards-container');
 
 
 navEmail.addEventListener('click', toggleDescktopMenu);
 menuMobile.addEventListener('click', toggleMobileMenu);
 shopingCar.addEventListener('click', toggleShoppingDetails);
+productDetailClose.addEventListener('click',closeProductDetail);
 
 function toggleDescktopMenu(){
     let isOpenshoppingDetails = shoppingDetails.classList.contains('inactive');
+    let isOpenProductDetails = productDetailContainer.classList.contains('inactive');
     
     if (!isOpenshoppingDetails){
         shoppingDetails.classList.add('inactive');  
+    }
+    if (!isOpenProductDetails){
+        productDetailContainer.classList.add('inactive');  
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
     let isOpenshoppingDetails = shoppingDetails.classList.contains('inactive');
+    let isOpenProductDetails = productDetailContainer.classList.contains('inactive');
     
     if (!isOpenshoppingDetails){
         shoppingDetails.classList.add('inactive');  
+    }
+    if (!isOpenProductDetails){
+        productDetailContainer.classList.add('inactive');  
     }
     toggleMobile.classList.toggle('inactive');
 }
@@ -32,42 +43,69 @@ function toggleMobileMenu(){
 function toggleShoppingDetails(){
     let isOpenMobileMenu = toggleMobile.classList.contains('inactive');
     let isOpenDesktopMenu = desktopMenu.classList.contains('inactive');
+    let isOpenProductDetails = productDetailContainer.classList.contains('inactive');
+
     if (!isOpenMobileMenu){
         toggleMobile.classList.add('inactive');  
     }
     if (!isOpenDesktopMenu){
         desktopMenu.classList.add('inactive');  
     }
+    if (!isOpenProductDetails){
+        productDetailContainer.classList.add('inactive');  
+    }
     shoppingDetails.classList.toggle('inactive');
     
 
     
 }
+function showProductDetail(){
+    let isOpenShoppingCart = shoppingDetails.classList.contains('inactive');
+    let isOpenDesktopMenu = desktopMenu.classList.contains('inactive');
+    let isOpenMobileMenu = toggleMobile.classList.contains('inactive');
+
+    if (!isOpenShoppingCart){
+        shoppingDetails.classList.add('inactive');  
+    }
+    if (!isOpenDesktopMenu){
+        desktopMenu.classList.add('inactive');  
+    }
+    if (!isOpenMobileMenu){
+        toggleMobile.classList.add('inactive');  
+    }
+    productDetailContainer.classList.remove('inactive');
+}
+function closeProductDetail(){
+    productDetailContainer.classList.add('inactive');
+}
+
+
 
 const productList = [];
+    productList.push({
+        name: 'Pantalla',
+        price: 220,
+        image: "https://img01.huaweifile.com/sg/ms/co/pms/uomcdn/CO_HW_B2C/pms/202205/gbom/6941487214969/800_800_CBDC68A805677FB27A9CECFEC129C80Fmp.png",
+    });
+    productList.push({
+        name: 'Computador',
+        price: 1000,
+        image: "https://d1zktoovjdma6n.cloudfront.net/media/catalog/product/cache/2868dafe2b4ec8474f3b444cfc8b532a/a/s/asusvivobook15_x1502za-bq242w_01.png",
+    });
+    productList.push({
+        name: 'Celular',
+        price: 540,
+        image: "https://www.kalley.com.co/medias/7705946476478-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMTYyODl8aW1hZ2UvanBlZ3xoZDcvaDRjLzEzNDMxMjEwNDQyNzgyLzc3MDU5NDY0NzY0NzhfMDAxXzc1MFd4NzUwSHxjYmFlZmVmMjViMjIwNTY4MzMyYWUyZjY5YWNiNDUzNjk2NjAzMGZmMGNlZjMwODdjNzc0OGE2ZmQyY2YxZjQz",
+    });
 
-productList.push({
-    name: 'Pantalla',
-    price: 220,
-    image: "https://img01.huaweifile.com/sg/ms/co/pms/uomcdn/CO_HW_B2C/pms/202205/gbom/6941487214969/800_800_CBDC68A805677FB27A9CECFEC129C80Fmp.png",
-});
-productList.push({
-    name: 'Computador',
-    price: 1000,
-    image: "https://d1zktoovjdma6n.cloudfront.net/media/catalog/product/cache/2868dafe2b4ec8474f3b444cfc8b532a/a/s/asusvivobook15_x1502za-bq242w_01.png",
-});
-productList.push({
-    name: 'Celular',
-    price: 540,
-    image: "https://www.kalley.com.co/medias/7705946476478-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMTYyODl8aW1hZ2UvanBlZ3xoZDcvaDRjLzEzNDMxMjEwNDQyNzgyLzc3MDU5NDY0NzY0NzhfMDAxXzc1MFd4NzUwSHxjYmFlZmVmMjViMjIwNTY4MzMyYWUyZjY5YWNiNDUzNjk2NjAzMGZmMGNlZjMwODdjNzc0OGE2ZmQyY2YxZjQz",
-});
-
-for (product of productList){
+function showProducts(arr){
+for (product of arr){
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     
     const img = document.createElement('img');
     img.setAttribute('src',product.image);
+    img.addEventListener('click',showProductDetail);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
@@ -93,4 +131,7 @@ for (product of productList){
     productCard.appendChild(productInfo);
 
     cardsContainer.appendChild(productCard);
+    }
 }
+
+showProducts(productList);
